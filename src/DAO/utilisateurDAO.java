4f0d91d9902defd04516475193be601061219052
Mattis <mnaud@la-joliverie.com>
2022-12-05 16:48:49 +0100

    Login necessary

--- src/DAO/utilisateurDAO.java
+++ src/DAO/utilisateurDAO.java
@@ -6,10 +6,15 @@
 package DAO;
 
 import METIERS.Utilisateur;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+
 
 /**
  *
@@ -39,7 +44,7 @@ public class utilisateurDAO {
             String mail = rs.getString("mailU");
             String mdp = rs.getString("mdpU");
             String pseudo = rs.getString("pseudoU");
-            int role = rs.getInt("role");
+            int role = rs.getInt("roleU");
             unUtilisateur = new Utilisateur (id, mail, mdp, pseudo, role);
             lesUtilisateurs.add(unUtilisateur);
         }
@@ -58,8 +63,41 @@ public class utilisateurDAO {
         pstmt.setInt(1, id);
         rs = pstmt.executeQuery();
         if(rs.next()){
-            unUtilisateur = new Utilisateur(rs.getInt("idU"), rs.getString("mailU"), rs.getString("mdpU"), rs.getString("pseudoU"), rs.getInt("role"));
+            unUtilisateur = new Utilisateur(rs.getInt("idU"), rs.getString("mailU"), rs.getString("mdpU"), rs.getString("pseudoU"), rs.getInt("roleU"));
         }
         return unUtilisateur;
+    }
+    
+    public static Utilisateur getOneByLogin(String mailU, String mdpU) throws SQLException, NoSuchAlgorithmException{
+        
+        Utilisateur unUtilisateur = null;
+        ResultSet rs = null;
+        PreparedStatement pstmt;
+        JDBC jdbc = JDBC.getInstance();
+        
+        
+        String request = "SELECT * FROM utilisateur WHERE mailU = ? AND mdpU = ?";
+        pstmt = jdbc.getConnexion().prepareStatement(request);
+        pstmt.setString(1, mailU);
+        pstmt.setString(2, getHash(mdpU).toString());
+        
+        rs = pstmt.executeQuery();
+        if(rs.next()){
+            unUtilisateur = new Utilisateur(rs.getInt("idU"), rs.getString("mailU"), rs.getString("mdpU"), rs.getString("pseudoU"), rs.getInt("roleU"));
+        }
+        return unUtilisateur;
+    }
+    
+    public static String getHash(String text) throws NoSuchAlgorithmException{
+        
+        MessageDigest crypt = MessageDigest.getInstance("SHA-256");
+        crypt.update(text.getBytes(StandardCharsets.UTF_8));
+        byte[] bytes = crypt.digest();
+        BigInteger bi = new BigInteger(1, bytes);
+        String digest = String.format("%0" + (bytes.length << 1) + "x", bi);
+        
+        System.out.println(digest);
+        
+        return digest;
     }
 }
--- src/TESTS/testUtilisateurDAO.java
+++ src/TESTS/testUtilisateurDAO.java
@@ -3,6 +3,7 @@ package TESTS;
 
 import DAO.JDBC;
 import METIERS.Utilisateur;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 
 public class testUtilisateurDAO {
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws NoSuchAlgorithmException{
         
         
         java.sql.Connection cnx = null;
@@ -20,6 +21,10 @@ public class testUtilisateurDAO {
             System.out.println("Test0 effectué : connexion\n");
             test1_getAll();
             System.out.println("Test1 effectué : sélection multiple\n");
+            test2_Login("spep", "spep");
+            System.out.println("Test3 effectué : Login utilisateur");
+            
+            
         } catch (SQLException e) {
             System.err.println("Erreur SQL : " + e);
         } finally {
@@ -37,15 +42,22 @@ public class testUtilisateurDAO {
         
     }
     
-    public static void test1_getAll() throws SQLException{
-        ArrayList<Utilisateur> lesUtilisateurs = DAO.utilisateurDAO.getAll();
-        System.out.println("Les Utilisateurs lus : " + lesUtilisateurs.toString());
-        }
+    
     
     public static void test0_Connexion() throws  SQLException {
         JDBC.creer("jdbc:mysql://193.26.14.51/", "resto", "user_resto", "ziG7@l1#sAE4");
         JDBC.getInstance().connecter();
         Connection cnx = JDBC.getInstance().getConnexion();
+    }
+    
+    public static void test1_getAll() throws SQLException { 
+        ArrayList<Utilisateur> lesUtilisateurs = DAO.utilisateurDAO.getAll();
+        System.out.println("Les Utilisateurs lus : " + lesUtilisateurs.toString());
+        }
+    
+    public static void test2_Login(String mailU, String mdpU) throws SQLException, NoSuchAlgorithmException {
+        Utilisateur unUtilisateur = DAO.utilisateurDAO.getOneByLogin(mailU, mdpU);
+        System.out.println("Un Login : " + unUtilisateur);
     }
     
 }
