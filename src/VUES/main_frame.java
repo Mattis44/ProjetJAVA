9ef272dc54c0972ab7caa4e7b3f55f7f4d6638d2
Mattis <mnaud@la-joliverie.com>
2023-02-27 17:38:25 +0100

    ajout recherche

--- src/VUES/main_frame.java
+++ src/VUES/main_frame.java
@@ -14,6 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import VUES.recherche_frame;
 
 /**
  *
@@ -47,6 +48,7 @@ public class main_frame extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         jButtonHideMsg = new javax.swing.JButton();
         jButtonDelMsg = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -80,6 +82,13 @@ public class main_frame extends javax.swing.JFrame {
 
         jButtonDelMsg.setText("Supprimer");
 
+        jButton1.setText("Recherche");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -89,7 +98,9 @@ public class main_frame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGap(6, 6, 6)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jButtonDelMsg, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButtonHideMsg, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -103,7 +114,8 @@ public class main_frame extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButtonHideMsg)
-                    .addComponent(jButtonDelMsg))
+                    .addComponent(jButtonDelMsg)
+                    .addComponent(jButton1))
                 .addContainerGap(15, Short.MAX_VALUE))
         );
 
@@ -116,6 +128,11 @@ public class main_frame extends javax.swing.JFrame {
         //}
     }//GEN-LAST:event_jButtonHideMsgActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        recherche_frame laVueRech = new recherche_frame();
+        laVueRech.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     public DefaultTableModel getModeleTable() {
         return modeleTable;
     }
@@ -125,6 +142,7 @@ public class main_frame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButtonDelMsg;
     private javax.swing.JButton jButtonHideMsg;
     private javax.swing.JScrollPane jScrollPane1;
